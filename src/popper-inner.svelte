c594c185dd2f91a08f271c847141aef6f769d934
Simon <simon.cheng@advantech.com.tw>
2022-06-21 10:50:00 +0800

    Fix popper display issue

---
+++ src/popper-inner.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { PAGE_STYLE } from "./constants/page-style.constant.svelte";
+  import type { Placement } from "@popperjs/core";
+
+  export let has_arrow: boolean | undefined = undefined;
+  export let popper_arrow: HTMLElement;
+  export let container_class: svelte.JSX.ClassName = "";
+  export let no_container: boolean | undefined = undefined;
+  export let current_placement: Placement | undefined = undefined;
+</script>
+
+{#if has_arrow}
+  <div
+    id="arrow"
+    class="self-center border-x-[6px] border-x-transparent border-b-[8px] border-b-white {PAGE_STYLE
+      .ARROW_PLACEMENT[current_placement] ?? ''}"
+    bind:this={popper_arrow}
+  />
+{/if}
+{#if no_container}
+  <slot />
+{:else}
+  <div class="relative z-50 w-full {container_class}"><slot /></div>
+{/if}
--- src/popper.svelte
+++ src/popper.svelte
@@ -2,10 +2,11 @@
   import { createPopper } from "@popperjs/core";
   import type { Placement, ModifierArguments } from "@popperjs/core";
   import { PAGE_STYLE } from "./constants/page-style.constant.svelte";
+  import PopperInner from "popper-svelte/src/popper-inner.svelte";
+  import { createEventDispatcher } from "svelte";
 
   export let reference: HTMLElement;
   export let id: string | undefined = undefined;
-  export let popper_class: svelte.JSX.ClassName = "";
   export let container_class: svelte.JSX.ClassName = "";
   export let popper_show: boolean | undefined = undefined;
   export let popper_class_show: 1 | undefined = undefined;
@@ -13,50 +14,43 @@
   export let has_arrow: boolean | undefined = undefined;
   export let no_container: boolean | undefined = undefined;
   export let current_placement: Placement | undefined = undefined;
-  export let insert_into_body: boolean | undefined = undefined;
   export let fixed: boolean | undefined = undefined;
+
+  let popper_class: svelte.JSX.ClassName = "";
+  export { popper_class as class };
 
   let popper: HTMLDivElement;
   let popper_arrow: HTMLElement;
   let show_local: boolean;
 
+  const dispatch = createEventDispatcher();
+
   export const toggle = (show?: boolean) => {
     popper_show = show ?? !popper_show;
 
     if (popper_show) {
-      if (insert_into_body) {
-        document.body.append(popper);
-        show_local = true;
-      }
-      setTimeout(
-        () => {
-          createPopper(reference, popper, {
-            placement,
-            ...(fixed ? { strategy: "fixed" } : {}),
+      setTimeout(() => {
+        createPopper(reference, popper, {
+          placement,
+          ...(fixed ? { strategy: "fixed" } : {}),
 
-            modifiers: [
-              {
-                name: "arrow",
-                options: {
-                  element: popper_arrow,
-                },
-                fn({ state }: ModifierArguments<any>) {
-                  current_placement = state.placement;
-                },
+          modifiers: [
+            {
+              name: "arrow",
+              options: {
+                element: popper_arrow,
               },
-            ],
-          });
-          setTimeout(() => {
-            popper_class_show = 1;
-          }, 0);
-        },
-        insert_into_body ? 100 : 0
-      );
+              fn({ state }: ModifierArguments<any>) {
+                current_placement = state.placement;
+              },
+            },
+          ],
+        });
+        setTimeout(() => {
+          popper_class_show = 1;
+        }, 0);
+      }, 0);
     } else {
-      if (insert_into_body) {
-        document.body.removeChild(popper);
-        show_local = false;
-      }
       popper_class_show = undefined;
     }
   };
@@ -68,21 +62,17 @@
     bind:this={popper}
     class="{popper_class} {has_arrow
       ? PAGE_STYLE.POPPER_PLACEMENT[current_placement]
-      : ''} {insert_into_body && !show_local ? 'hidden' : ''} z-[99999]"
+      : ''} !z-[99999]"
     class:opacity-0={!popper_class_show}
+    on:mouseenter={() => {
+      dispatch("mouseenter");
+    }}
+    on:mouseleave={() => {
+      dispatch("mouseleave");
+    }}
   >
-    {#if has_arrow}
-      <div
-        id="arrow"
-        class="self-center border-x-[6px] border-x-transparent border-b-[8px] border-b-white {PAGE_STYLE
-          .ARROW_PLACEMENT[current_placement] ?? ''}"
-        bind:this={popper_arrow}
-      />
-    {/if}
-    {#if no_container}
+    <PopperInner {...$$restProps}>
       <slot />
-    {:else}
-      <div class="relative z-50 {container_class}"><slot /></div>
-    {/if}
+    </PopperInner>
   </div>
 {/if}
